2da14bc37fec238d02a2a45b39a785bbde2931ea
OgniyaMarch <angel13osetrova@gmail.com>
2023-12-09 14:20:42 +0200

    create project with mongoDB dependency

---
+++ src/main/java/com/example/EVeteranBot/EVeteranBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.EVeteranBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVeteranBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVeteranBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EVeteranBot/EVeteranBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EVeteranBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVeteranBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
